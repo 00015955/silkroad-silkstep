2bc0b75179750b4feb3f8f74af8b4d7c2766152a
00015955 <00015955wiut@gmail.com>
2026-04-25 04:41:58 +0500

    filter

--- silkroad-silkstep/src/main/java/uz/silkStep/project/filter/BaseFilter.java
+++ silkroad-silkstep/src/main/java/uz/silkStep/project/filter/BaseFilter.java
@@ -8,3 +8,7 @@ public class BaseFilter {
     private Integer page = 0;
     private Integer size = 9;
 }
+
+// BaseFilter → serves as a base class for filtering and pagination in API requests. 
+//It contains common fields such as search (for keyword-based filtering), page (for pagination), and size (to specify the number of items per page). 
+//This class can be extended by specific filter classes for different entities, allowing for consistent handling of filtering and pagination across the application.
--- silkroad-silkstep/src/main/java/uz/silkStep/project/filter/UserFilter.java
+++ silkroad-silkstep/src/main/java/uz/silkStep/project/filter/UserFilter.java
@@ -12,3 +12,8 @@ public class UserFilter extends BaseFilter {
     private UserStatus status;
     private List<UserStatus> statuses;
 }
+
+
+// UserFilter → represents a filter object for querying users, containing fields for a single UserStatus (status) and a list of UserStatus values (statuses). 
+//This filter is used to specify criteria for filtering users based on their status when retrieving user data from the database. 
+//The presence of both a single status and a list of statuses allows for flexible querying, enabling the retrieval of users that match either a specific status or any of the statuses in the provided list.
